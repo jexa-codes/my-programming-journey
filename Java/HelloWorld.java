1c616ce25cf605a40bbe36f19ab03c730efcebe8
jexa-codes <jeevitha.r2038@gmail.com>
2026-07-28 15:26:04 +0530

    Update HelloWorld.java

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello GitHub!");
+        System.out.println("My name is Jeevitha.");
+        System.out.println("I'm learning GitHub!");
     }
 }
